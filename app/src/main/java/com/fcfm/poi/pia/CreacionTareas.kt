10013cc38a949d1aabeac09af9287dd9bf0d74b8
Jose Antonio Parga Tolano <parga.jose@outlook.com>
2020-11-04 22:24:20 -0600

    Pantalla de creacion de tareas
    Task creation screen

---
+++ app/src/main/java/com/fcfm/poi/pia/CreacionTareas.kt
@@ -0,0 +1,12 @@
+package com.fcfm.poi.pia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreacionTareas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creacion_tareas)
+    }
+}
