74e06d2ef79b869b44076cd1ec930a18237d398e
Jose Antonio Parga Tolano <parga.jose@outlook.com>
2020-11-04 23:17:22 -0600

    Pantalla de tareas a medio hacer
    pantalla de vista de tareas fue creada, commit de emergencia para descargar nueva version de android studio

---
+++ app/src/main/java/com/fcfm/poi/pia/modelos/Assignment.kt
@@ -0,0 +1,7 @@
+package com.fcfm.poi.pia.modelos
+
+class Assignment (var id: String = "",
+                  var titulo: String = "",
+                  var instrucciones: String = "",
+                  val timeStamp: Any? = null){
+}
