9dedde6db84e4d544e897f0bd1368e62c4605b40
Lucia Guadalupe Domínguez De la Cruz <61896758+LuciaDominguez@users.noreply.github.com>
2020-11-05 00:01:50 -0600

    Diseño de chat
    Diseño de burbuja y de chat

--- app/src/main/java/com/fcfm/poi/pia/loginActivity.kt
+++ app/src/main/java/com/fcfm/poi/pia/loginActivity.kt
@@ -1,11 +1,18 @@
 package com.fcfm.poi.pia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        viewRegistrate.setOnClickListener {
+           val intentLogin = Intent(this,registerActivity::class.java)
+            startActivity(intentLogin)
+        }
     }
 }
--- app/src/main/java/com/fcfm/poi/pia/registerActivity.kt
+++ app/src/main/java/com/fcfm/poi/pia/registerActivity.kt
@@ -1,11 +1,18 @@
 package com.fcfm.poi.pia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class registerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnRegistrarse.setOnClickListener {
+            val intentRegistro = Intent(this, loginActivity::class.java)
+            startActivity(intentRegistro)
+        }
     }
 }
