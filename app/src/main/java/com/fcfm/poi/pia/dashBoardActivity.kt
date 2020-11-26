46d71bfebe74f27d686d609fb43aba0cce80dd5b
Lucia Guadalupe Domínguez De la Cruz <61896758+LuciaDominguez@users.noreply.github.com>
2020-11-26 17:52:42 -0600

    Ventana de inicio
    Se diseño la ventana de inicio, falta el buscador

---
+++ app/src/main/java/com/fcfm/poi/pia/dashBoardActivity.kt
@@ -0,0 +1,13 @@
+package com.fcfm.poi.pia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+
+class dashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+
+}
