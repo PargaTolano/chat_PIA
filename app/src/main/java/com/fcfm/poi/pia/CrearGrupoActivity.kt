7dec8367834ac12a97764aa7d1bc5b9e05715477
Lucia Guadalupe Domínguez De la Cruz <61896758+LuciaDominguez@users.noreply.github.com>
2020-12-03 18:35:33 -0600

    Pantalla crear grupo
    Tiene un recycle view para ver los integrantes

---
+++ app/src/main/java/com/fcfm/poi/pia/CrearGrupoActivity.kt
@@ -0,0 +1,11 @@
+package com.fcfm.poi.pia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearGrupoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_grupo)
+    }
+}
