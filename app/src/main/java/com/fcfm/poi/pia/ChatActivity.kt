1abf29e6e3a3536d627d2a6e0aca7e3c6ef9499b
Romo <romo1499@hotmail.com>
2021-01-07 02:30:04 -0600

    imagenes en mensajes
    v

--- app/src/main/java/com/fcfm/poi/pia/ChatActivity.kt
+++ app/src/main/java/com/fcfm/poi/pia/ChatActivity.kt
@@ -1,11 +1,13 @@
 package com.fcfm.poi.pia
 
+import android.Manifest
 import android.app.Activity
 import android.app.ProgressDialog
-import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -17,40 +19,49 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_chat.*
-import kotlinx.android.synthetic.main.activity_login.*
-import java.net.URI
+import java.io.ByteArrayOutputStream
 
 class ChatActivity : AppCompatActivity() {
 
-    private val listaMensajes = mutableListOf<Mensaje>();
-    private val adaptador = ChatAdapter(listaMensajes);
-    private lateinit var nombreUsuario: String;
-    private lateinit var chatroomId : String;
-    private var filepath : Uri = Uri.EMPTY;
-    private val database  = FirebaseDatabase.getInstance();
-    private val chatroomRef = database.getReference("chatrooms");
-    private lateinit var chatRef : DatabaseReference;
+    private val listaMensajes = mutableListOf<Mensaje>()
+    private lateinit var nombreUsuario: String
+    private lateinit var chatroomId : String
+    private lateinit var adaptador: ChatAdapter
+    private var filepath : Uri = Uri.EMPTY
+    private var file : ByteArray? = null
+    private val database  = FirebaseDatabase.getInstance()
+    private val chatroomRef = database.getReference("chatrooms")
+    private lateinit var chatRef : DatabaseReference
 
-    private val currUser = FirebaseAuth.getInstance().currentUser;
+    private val currUser = FirebaseAuth.getInstance().currentUser
 
+
+    companion object{
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000
+        //Permission code
+        private val PERMISSION_CODE = 1001
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        nombreUsuario = intent.getStringExtra("nombreUsuario") ?: "sin_nombre";
+        nombreUsuario = intent.getStringExtra("nombreUsuario") ?: "sin_nombre"
 
-        chatroomId  = intent.getStringExtra("chatroomId") ?: "";
+        chatroomId  = intent.getStringExtra("chatroomId") ?: ""
+
+        adaptador = ChatAdapter(listaMensajes, chatroomId)
 
         if(chatroomId.isEmpty()){
-            finish();
+            finish()
         }
 
-        chatRef = chatroomRef.child("${chatroomId}/chat");
+        chatRef = chatroomRef.child("${chatroomId}/chat")
 
-        nombreUChat.text = nombreUsuario;
+        nombreUChat.text = nombreUsuario
 
-        rvChat.adapter = adaptador;
+        rvChat.adapter = adaptador
 
         btnEnviar.setOnClickListener {
 
@@ -59,7 +70,14 @@ class ChatActivity : AppCompatActivity() {
 
                 txtMensaje.text.clear()
 
-                enviarMensaje(Mensaje("", mensaje, currUser?.email!! , ServerValue.TIMESTAMP))
+                if(filepath != Uri.EMPTY)
+                {
+                    enviarMensaje(Mensaje("", mensaje, file, currUser?.email!! , ServerValue.TIMESTAMP))
+                }
+                else{
+                    enviarMensaje(Mensaje("", mensaje, null, currUser?.email!! , ServerValue.TIMESTAMP))
+                }
+                filepath = Uri.EMPTY
             }
         }
 
@@ -71,39 +89,77 @@ class ChatActivity : AppCompatActivity() {
 
         goBack.setOnClickListener {
             //val intentLogin = Intent(this,registerActivity::class.java)
-            finish();
+            finish()
             //val intent = Intent(this, dashBoardActivity::class.java)
             //startActivity(intent)
         }
 
         imageView.setOnClickListener {
             startFileChooser()
-            uploadFile()
+            //uploadFile()
         }
 
+        listaMensajes.clear()
         recibirMensajes()
     }
 
     private fun startFileChooser()
     {
-        var i = Intent()
-        i.setType("image/*")
-        i.setAction(Intent.ACTION_GET_CONTENT)
-        startActivityForResult(Intent.createChooser(i, "Choose picture"), 111)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            var boolDo:Boolean =  false
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED){
+                //permission denied
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                //show popup to request runtime permission
+                requestPermissions(permissions, PERMISSION_CODE)
+            }
+            else{
+                //permission already granted
+                boolDo =  true
+
+            }
+
+
+            if(boolDo == true){
+                pickImageFromGallery()
+            }
+
+        }else{
+            pickImageFromGallery()
+        }
+    }
+
+
+    private fun pickImageFromGallery()
+    {
+
+        val intent  =  Intent()
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.type = "image/*"
+        startActivityForResult(Intent.createChooser(intent,"Selecciona"),
+            ChatActivity.IMAGE_PICK_CODE
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==111 && resultCode == Activity.RESULT_OK && data != null )
+        if(requestCode==ChatActivity.IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK && data != null )
         {
-            filepath =  data.data!!
-            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filepath)
-            enviarFoto(Imagen("", bitmap))
-            imageView.setImageBitmap(bitmap)
+
+            filepath= data.data!!
+            var  photo = MediaStore.Images.Media.getBitmap(contentResolver,filepath)
+            //val photo: Bitmap? =  data?.extras?.get("data") as Bitmap
+            val stream = ByteArrayOutputStream()
+            //Bitmap.CompressFormat agregar el formato desado, estoy usando aqui jpeg
+            photo?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            file =  stream.toByteArray()
         }
     }
 
-    private fun uploadFile()
+    private fun uploadFile(id: String)
     {
         if(filepath!=Uri.EMPTY)
         {
@@ -111,7 +167,7 @@ class ChatActivity : AppCompatActivity() {
             pd.setTitle("Uploading")
             pd.show()
 
-            var imageRef = FirebaseStorage.getInstance().reference.child("chatroom/"+chatRef+"pic.jpg")
+            var imageRef = FirebaseStorage.getInstance().reference.child("chatroom/"+chatRef+"/"+id+"pic.jpg")
             imageRef.putFile(filepath)
                 .addOnSuccessListener { p0 ->
                     pd.dismiss()
@@ -128,40 +184,39 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 
-    private fun enviarFoto(imagen: Imagen) {
-
-        val mensajeFireBase = chatRef.push()
-        imagen.id = mensajeFireBase.key ?: ""
-
-        mensajeFireBase.setValue(imagen)
-    }
-
     private fun enviarMensaje(mensaje: Mensaje) {
 
         val mensajeFireBase = chatRef.push()
         mensaje.id = mensajeFireBase.key ?: ""
 
+        uploadFile(mensaje.id)
+
         mensajeFireBase.setValue(mensaje)
+
+        listaMensajes.clear()
     }
 
     private fun recibirMensajes() {
         chatRef.addValueEventListener(object: ValueEventListener{
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                listaMensajes.clear();
+
+                listaMensajes.clear()
+
                 for (snap in snapshot.children)
                 {
-                    val mensaje: Mensaje = snap.getValue(Mensaje::class.java) as Mensaje;
+                    val mensaje: Mensaje = snap.getValue(Mensaje::class.java) as Mensaje
 
                     if(mensaje.de == currUser?.email!!){
-                        mensaje.esMio = true;
+                        mensaje.esMio = true
                     }
 
-                    listaMensajes.add(mensaje);
+                    listaMensajes.add(mensaje)
                 }
 
                 if(listaMensajes.size > 0) {
-                    adaptador.notifyDataSetChanged();
+                    adaptador.notifyDataSetChanged()
+
                 }
             }
 
--- app/src/main/java/com/fcfm/poi/pia/modelos/Mensaje.kt
+++ app/src/main/java/com/fcfm/poi/pia/modelos/Mensaje.kt
@@ -5,8 +5,9 @@ import com.google.firebase.database.Exclude
 class Mensaje(
     var id: String = "",
     var contenido: String = "",
+    @get:Exclude var archivo: ByteArray? = null,
     var de: String = "",
-    val timeStamp: Any? = null
+    var timeStamp: Any? = null
 ) {
     @Exclude
     var esMio: Boolean = false
